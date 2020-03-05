a1cd53c25276912fdc320671b521dee87d435ee1
Jennifer Miller <jmillerarcher@me.com>
2020-03-05 15:00:05 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,15 +10,18 @@ class PostsController < ApplicationController
   def update
    
     
+    @post = Post.find(params[:id])
+
+
     if @post.update(post_params)
-
       
-    redirect_to post_path(@post)	    else
+      redirect_to post_path(@post)
+      
+    else
+      
       render :edit
-    end
-    
-
-    redirect_to post_path(@post)
+      
+    end 
   end
 
   private
